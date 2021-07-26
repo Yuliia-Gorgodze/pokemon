3ae0bdf6dd4277a101cf7740e0b7ff2d6e3dbe0f
Yuliia-Gorgodze <taragorgodze@gmail.com>
2021-07-26 22:10:11 +0300

    delete coment

--- src/components/ModalPokemonInfo.js
+++ src/components/ModalPokemonInfo.js
@@ -2,37 +2,12 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import style from './styles/modal.module.css'
 import typeColors from '../helpers/color'
-import selectors from '../redux/pokemon/pokemon-selectors'
-import selectorsPage from '../redux/page/pageSelectors'
-import {useDispatch, useSelector} from 'react-redux'
-import operations from '../redux/pokemon/pokemon-operations'
 import { nanoid } from 'nanoid'
 import { CloseOutlined } from '@ant-design/icons';
-import {HeartTwoTone,  DeleteTwoTone } from '@ant-design/icons';
+
 
 export default function Modal({ open, onClose, pokemon }) {
-  const favoritPokemonId = useSelector(selectors.getFavoritePokemon).map(el => el.id)
-  const page = useSelector(selectorsPage.getPage)
-  const dispatch = useDispatch();
-  const handleChange = (e) =>{
-    console.log();
- if(page ==='gallery' &&  e.target.nodeName === 'BUTTON'){
-  e.target.classList.toggle('buttonAddFavorite')
-}
-if(e.target.nodeName === 'BUTTON'){
-    addFavorite(pokemon)
-}
-return
-}
-const addFavorite  = (pokemon) => {
-  console.log(favoritPokemonId);
-  if(favoritPokemonId.includes(pokemon.id)){
-      dispatch(operations.deleteFavoritePokemon(pokemon.id))
-      return
-  }else{
-      dispatch(operations.addFavoritePokemon(pokemon))
-  }  
-} 
+
   if (!open) return null
   console.dir(pokemon.pokemonUrl);
   return ReactDom.createPortal(
